4cb478e6d99f12ee37fa31291abc401a71e04f77
lifan-kubuntu <lifandream@gmail.com>
2016-11-18 16:55:38 +0900

    20161118 search test

--- scrach2.py
+++ scrach2.py
@@ -24,14 +24,9 @@ def getdata(input_file):
     fin = open(input_file)
     fin_lines = fin.readlines()
     fin.close()
-    data_dicts = {}
-    line_num = 0
+    data_books = []
 
-    for fin_line in fin_lines:
-        fin_list = fin_line.split(",")
-        fin_num = fin_list[0]
-        fin_str = fin_list[1]
-        line_num += 1
+    for fin_str in fin_lines:
         #中間XMLを生成する
         cmd = 'java -jar ASA20150617.jar -x'
         cmd_str = res_cmd(cmd, fin_str.encode(default_encoding))
@@ -76,31 +71,29 @@ def getdata(input_file):
                     else:
                         data_list[1]['link'+tmp_dict['link'] ] = tmp_dict['noun_surface']
 
-        if len(fin_list) > 2:
-            data_dicts["問"+str(line_num)] = data_lists
-            answer["問"+str(line_num)] = fin_list[2]
-            if textbook_list[fin_num] == data_lists:
-                result["問"+str(line_num)] = 1
-            else:
-                result["問"+str(line_num)] = 0
-            if result["問"+str(line_num)] == answer["問"+str(line_num)]:
-                score += 1
-        else:
-            data_dicts[fin_num] = data_lists
+        data_books.append(data_lists)
+    return data_books
 
-    return data_dicts
+def output_result(textbook_txt, question_txt, output_txt):
+    textbooks = getdata(textbook_txt)
+    questions = getdata(question_txt)
+    result = []
+    fin = open(question_txt)
+    fin_lines = fin.read().splitlines()
+    fin.close()
+    fout = codecs.open(output_txt, 'w', 'utf-8')
+    for question in questions:
+        if question in textbooks:
+            result.append('1')
+        else:
+            result.append('0')
+    for idx, val in enumerate(fin_lines):
+        fout.write(val+','+result[idx]+'\n')
 
 #----------------------------------------------main
-result = {}
-answer = {}
-score = 0
-textbook_list = getdata('input.txt')
-question_list = getdata('question.txt')
-print(textbook_list)
-print(question_list)
-print(result)
-print(answer)
-print("点数："+str(score))
+
+output_result('input.txt', 'question.txt', 'output.txt')
+
 
 
 
